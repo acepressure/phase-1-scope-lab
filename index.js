3020e0920672dcfe5caa73f6f67e588339001bb8
Walter Simon <simonwalter986@gmail.com>
2022-06-20 15:27:54 +0300

    commit tree b6df9f82d97d245f00d91e0aa34b28b536e30aea

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+customerName = 'bob';
+
+function customer() {
+return customerName;
+}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+     bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    let leastFavoriteCustomer = 'any';
+    leastFavoriteCustomer = 'walter'; 
+}
